f9cab81962f61ab66e81ad025f486be75510cf6e
john previtera <johngprevitera@gmail.com>
2024-06-13 11:07:12 -0600

    registered schemas in dbcontext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { ChatSchema } from '../models/Chat.js';
+import { MessageSchema } from '../models/Message.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Chat = mongoose.model('Chat', ChatSchema);
+  Message = mongoose.model('Message', MessageSchema);
 }
 
 export const dbContext = new DbContext()
