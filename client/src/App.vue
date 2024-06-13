96e647e1f0ef06c38778f139f884e38dfa72bdb2
john previtera <johngprevitera@gmail.com>
2024-06-13 11:41:16 -0600

    logo loaded

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,13 +7,13 @@ import Navbar from './components/Navbar.vue'
 
 <template>
   <header>
-    <Navbar />
+    <Logo />
   </header>
   <main>
     <router-view />
   </main>
   <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
+
   </footer>
 </template>
 
@@ -24,6 +24,13 @@ import Navbar from './components/Navbar.vue'
   --main-height: calc(100vh - 32px - 64px);
 }
 
+main {
+  background-color: #6C805D;
+}
+
+header {
+  background-color: #6C805D;
+}
 
 footer {
   display: grid;
---
+++ client/src/components/Logo.vue
@@ -0,0 +1,20 @@
+<script setup>
+import rdacted from '../assets/img/rdacted.svg'
+</script>
+
+
+<template>
+    <div class="ps-3">
+        <img :src="rdacted" alt="">
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+span {
+    font-family: "Libre Barcode 128 Text", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 64px;
+}
+</style>
