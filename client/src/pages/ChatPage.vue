699db528de117f0201fa33bd6c2485d320c56668
john previtera <johngprevitera@gmail.com>
2024-07-29 13:39:03 -0600

    removed text area when not logged in.

--- client/src/pages/ChatPage.vue
+++ client/src/pages/ChatPage.vue
@@ -40,10 +40,14 @@ onMounted(() => {
             <div class="col-12 d-flex flex-column textbox chatlog justify-content-end">Chatlog goes here</div>
         </section>
 
-        <section class="row px-1 mt-3 m-1 text-light justify-content-between align-items-center">
+        <section class="row px-1 mt-3 m-1 text-light align-items-center">
             <UserIcon :user="user" />
-            <textarea class="col-8 textbox text-light" rows="5"></textarea>
-            <button class="col-2 text-light"><i class="mdi mdi-send"></i></button>
+            <form v-if="user" @submit="sendMessage()" class="col-10 ">
+                <div class="row justify-content-between align-items-center">
+                    <textarea class="textbox col text-light" rows="5"></textarea>
+                    <button class="text-light"><i class="mdi mdi-send"></i></button>
+                </div>
+            </form>
         </section>
 
     </section>
@@ -57,5 +61,9 @@ onMounted(() => {
 
 .chatlog {
     height: 50vh;
+}
+
+button {
+    width: 10vh;
 }
 </style>
